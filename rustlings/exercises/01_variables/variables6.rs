a78af6ea8964a5b1a91637459df7a91e0af4fb28
André Pinotti <115902096+andrepinotti@users.noreply.github.com>
2025-05-15 21:48:59 -0300

    fix variables6.rs

--- rustlings/exercises/01_variables/variables6.rs
+++ rustlings/exercises/01_variables/variables6.rs
@@ -1,5 +1,6 @@
-// TODO: Change the line below to fix the compiler error.
-const NUMBER = 3;
+// DONE: Change the line below to fix the compiler error.
+// ERROR -> the constant 'number' isn't typed, append i32 to constant
+const NUMBER: i32 = 3;
 
 fn main() {
     println!("Number: {NUMBER}");
