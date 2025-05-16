c5138b040de37d4daac61a1b381c69bba20e9d83
André Pinotti <115902096+andrepinotti@users.noreply.github.com>
2025-05-15 21:33:09 -0300

    fix variables4.rs

--- rustlings/exercises/01_variables/variables4.rs
+++ rustlings/exercises/01_variables/variables4.rs
@@ -1,8 +1,10 @@
-// TODO: Fix the compiler error.
+// DONE: Fix the compiler error.
 fn main() {
     let x = 3;
     println!("Number {x}");
 
-    x = 5; // Don't change this line
+    // x = 5; // Don't change this line
+    let x = 5;
+    //FIXME -> ERROR: the variable x must be redeclare with let in the beginning
     println!("Number {x}");
 }
