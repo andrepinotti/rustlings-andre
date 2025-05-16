2e4ee4d9017e2a66416e7845c862154c65c67878
André Pinotti <115902096+andrepinotti@users.noreply.github.com>
2025-05-15 21:36:41 -0300

    fix variables5.rs

--- rustlings/exercises/01_variables/variables5.rs
+++ rustlings/exercises/01_variables/variables5.rs
@@ -2,7 +2,8 @@ fn main() {
     let number = "T-H-R-E-E"; // Don't change this line
     println!("Spell a number: {}", number);
 
-    // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+    // DONE: Fix the compiler error by changing the line below without renaming the variable.
+    // The type of the 'number' variable is str, then we have redeclare her
+    let number = 3;
     println!("Number plus two is: {}", number + 2);
 }
