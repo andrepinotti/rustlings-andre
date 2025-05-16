5fc9e87099d6ef6125af6251fff94e3b2e04a775
André Pinotti <115902096+andrepinotti@users.noreply.github.com>
2025-05-15 21:26:39 -0300

    fix variables3.rs

--- rustlings/exercises/01_variables/variables3.rs
+++ rustlings/exercises/01_variables/variables3.rs
@@ -1,6 +1,7 @@
 fn main() {
-    // TODO: Change the line below to fix the compiler error.
-    let x: i32;
+    // DONE: Change the line below to fix the compiler error.
+    // Error: the variable x isn't initialized 
+    let x: i32 = 5;
 
     println!("Number {x}");
 }
