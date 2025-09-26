322bb491dee596257774e695623b56d1fd65466b
André Pinotti <115902096+andrepinotti@users.noreply.github.com>
2025-09-26 12:03:06 -0300

    fix: quiz2

--- rustlings/exercises/quizzes/quiz2.rs
+++ rustlings/exercises/quizzes/quiz2.rs
@@ -24,10 +24,28 @@ enum Command {
 }
 
 mod my_module {
+    use std::sync::Arc;
+
     use super::Command;
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<(String)> {
+          input
+            .into_iter()
+            .map(|(mut string, command)| {
+                match command {
+                    Command::Uppercase => string.to_uppercase(),
+                    Command::Trim => string.trim().to_string(),
+                    Command::Append(n) => {
+                        string.push_str(&"bar".repeat(n));
+                        string
+                    }
+                }
+            })
+            .collect()
+    }
+
 }
 
 fn main() {
@@ -38,8 +56,9 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
-
+    
     #[test]
     fn it_works() {
         let input = vec![
